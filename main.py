1c31d4a1d4cff4caa3493e21f62a271238928c5e
raphalopess <raphael.lopes.2013@gmail.com>
2022-02-08 19:27:44 -0300

    exito

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import calendar
-from tkinter.tix import COLUMN
 
 text=calendar.calendar(2019)
 
@@ -9,3 +8,7 @@ root.geometry("500x600")
 root.title("CALENDAR")
 label1 = Label(root, text="CALENDAR",bg='dark gray',font=("times",28,'bold'))
 label1.grid(row=1, column=1)
+root.config(background='white')
+l1=Label(root,text=text,font="Consoles 10")
+l1.grid(row=2,column=1, padx=20)
+root.mainloop()
